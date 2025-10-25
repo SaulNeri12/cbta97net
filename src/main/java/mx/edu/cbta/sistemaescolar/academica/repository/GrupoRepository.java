e6890c1e0c19b0085860f3cd0732927c8fb02c43
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-10-24 23:38:40 -0600

    Validacion de Letra de Grupo
    Cuando un grupo de un grado X se va a registrar, primero se evalua si ya existe un grupo del mismo grado con dicha letra en el mismo ciclo escolar.

--- src/main/java/mx/edu/cbta/sistemaescolar/academica/repository/GrupoRepository.java
+++ src/main/java/mx/edu/cbta/sistemaescolar/academica/repository/GrupoRepository.java
@@ -1,10 +1,14 @@
 package mx.edu.cbta.sistemaescolar.academica.repository;
 
 import mx.edu.cbta.sistemaescolar.academica.model.Grupo;
+import mx.edu.cbta.sistemaescolar.curricular.model.CicloEscolar;
+import mx.edu.cbta.sistemaescolar.curricular.model.Grado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
     List<Grupo> findByCicloEscolar_Id(Long idCicloEscolar);
+
+    Grupo findBySemestreAndLetraAndCicloEscolar(Grado semestre, Character letra, CicloEscolar cicloEscolar);
 }
