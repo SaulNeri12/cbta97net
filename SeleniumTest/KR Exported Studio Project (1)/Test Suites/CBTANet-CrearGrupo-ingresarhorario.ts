030cc9fb217a3c7eed1d16c864d4a8cff75f50f7
fabricio01352 <aldacoaguilerafabricio@gmail.com>
2025-11-06 00:18:36 -0700

    selenium

---
+++ SeleniumTest/KR Exported Studio Project (1)/Test Suites/CBTANet-CrearGrupo-ingresarhorario.ts
@@ -0,0 +1,161 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CBTANet-CrearGrupo-ingresarhorario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1ddb8793-0b3d-4062-85a1-ded578ca3e7d</testSuiteGuid>
+   <testCaseLink>
+      <guid>c303b04c-cece-407e-89c1-0f5111823df6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ciclo escolar activo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>9a0b8b71-7e7b-46a1-b9d4-8d2cd02147c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ciclo Escolar Inactivo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>ae841b29-06dd-4015-890b-b2297d83846c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Mostrar Turnos Disponibles</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>fc69532b-78b4-438e-87d8-7a5449bd5394</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar caracteres invalidos en letra del grupo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>55aceba3-5ce7-4490-8174-3932ce66dc8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Mostrar tabla de horarios con clases agregadas</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>faa19f79-1689-49a4-8749-1bbff94223a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar Nota de Grupo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>21a6b0b6-8808-4029-9e01-fe5a1811989c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Mostrar carrera tecnica</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>b2887569-c2b4-4307-8aa3-d9c86230b9a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Mostrar area propedeutica</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>ecea2474-9f9b-4011-9307-7f000dbb1351</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Mostrar menú para agregar clase</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>0a619ebe-c283-4614-b459-65500a74eb92</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Crear clase sin aula</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>b2600957-026e-4f3f-8270-f3dc475098f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Crear clase sin docente</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>0a5b15e4-027e-46de-bc12-a0cb4f4d2570</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar Horario de clase</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>a5f788a8-1a45-4eea-aa11-ab1a5268b325</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar horas de mas por materia</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>3b3735f4-9db7-4de2-8822-ca6788510aa4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar menos horas por materia que las adecuadas</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>a58866f0-ba6a-4129-b88a-0c666a3c8a74</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar clase en tabla de horario de grupo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>e6704eb1-1e09-400b-9851-2977f0595976</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Visualizar materias disponibles por semestre</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>a6dac76d-cc9c-4206-949d-a10d87571b62</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Visualizar materias disponibles por semestre y carrera técnica</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>4c2de638-9e80-4e74-a507-727656456350</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Visualizar materias disponibles por semestre, carrera técnica y área propedéutica</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>75dde1ff-31e6-418b-80ea-f37a42964fbe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Validación de campos obligatorios</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>f7c88dd2-0054-4474-a478-adaea805371f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Evitar materias duplicadas</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>75bd9d0e-5815-49f6-a592-859de8a7c509</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Cancelación del proceso</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>d6876720-1f28-4c07-8a67-aa1c2d71cd70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Crear clases en otro momento</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>00b0a868-5095-494d-b14b-92547644020d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Asignar Aula Ocupada</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>e5fb63e6-dfde-48c3-9a7b-517d18bc037e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ocultar materia después de agregarla a la tabla de horario</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>ce4b9b60-1b52-460b-93ba-6150f9814679</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Editar clase Asignada</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>2ad3695f-6000-46a8-8943-c8b794cac220</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Eliminar clase asignada</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>9add3341-360f-4cb2-a87d-58befe7dda6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Ingresar nota de grupo invalida</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>52972bad-82d9-4790-876c-e2dd0f9e84dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Crear grupo Completo</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>718548cc-7377-41b9-8f3a-eb23d1fa82d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CBTANet-CrearGrupo-ingresarhorario/Asignar docente ocupado</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
