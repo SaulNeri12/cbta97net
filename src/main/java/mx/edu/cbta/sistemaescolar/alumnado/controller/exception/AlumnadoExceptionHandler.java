5e3d2425fee663d9601dbcb261fb57c47043fd08
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2025-11-09 17:22:17 -0700

    Ya jalo 100%

--- src/main/java/mx/edu/cbta/sistemaescolar/alumnado/controller/exception/AlumnadoExceptionHandler.java
+++ src/main/java/mx/edu/cbta/sistemaescolar/alumnado/controller/exception/AlumnadoExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException; // <-- IMPORTAR
 
 import java.io.IOException;
 import java.util.Map;
@@ -34,8 +34,10 @@ public class AlumnadoExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Argumento inválido: " + ex.getMessage()));
     }
 
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<Map<String, String>> handleMaxSizeException(MaxUploadSizeExceededException exc) {
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(Map.of("error", "El archivo es demasiado grande."));
+        // Devuelve un error 413 (Payload Too Large)
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(Map.of("error", "El archivo es demasiado grande. El límite es de 10MB."));
     }
 }
